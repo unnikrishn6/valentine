1be755bf9931bf27fd35ab25d18b71429e47fea8
unnikrishn6 <unnikrishn6@gmail.com>
2026-02-13 08:06:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure buu?",
+      "Really sure bubuuu?",
+      "Think again!",
+      "Surely not?",
+      "You might regret this!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Im paavo di chellooo",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart",
+      "Is that your final answer?",
+      "You're breaking my heart",
+      "Please buuuu? You're breaking my heart",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="text-3xl md:text-5xl font-bold my-4">
+            Thanks di chellooo! 
+            <p>Happy Valentine's Day!</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[230px] rounded-lg shadow-lg"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.webp"
+          />
+          <h1 className="text-4xl md:text-6xl my-4 text-center">
+            Will you be my Valentine?
+          </h1>
+          <div className="flex flex-wrap justify-center gap-2 items-center">
+            <button
+              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <a
+      className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      target="__blank"
+    >
+      Made with Dudu
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>
+    </a>
+  );
+};
